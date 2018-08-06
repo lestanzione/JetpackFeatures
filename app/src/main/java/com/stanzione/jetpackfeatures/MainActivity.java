4cd50209e3e5f75757cfc9f7cd5fd1a4b6b13e4b
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-08-06 11:46:27 -0300

    Activity without feature

--- app/src/main/java/com/stanzione/jetpackfeatures/MainActivity.java
+++ app/src/main/java/com/stanzione/jetpackfeatures/MainActivity.java
@@ -2,12 +2,49 @@ package com.stanzione.jetpackfeatures;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView firstNameTextView;
+    private TextView lastNameTextView;
+    private TextView ageTextView;
+    private Button increaseAgeButton;
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createUser();
+        setupUi();
+
     }
+
+    private void createUser(){
+        user = new User("Leandro", "Stanzione");
+    }
+
+    private void setupUi(){
+        firstNameTextView = findViewById(R.id.firstNameTextView);
+        lastNameTextView = findViewById(R.id.lastNameTextView);
+        ageTextView = findViewById(R.id.ageTextView);
+        increaseAgeButton = findViewById(R.id.increaseAgeButton);
+
+        firstNameTextView.setText(user.getFirstName());
+        lastNameTextView.setText(user.getLastName());
+        ageTextView.setText(String.valueOf(user.getAge()));
+
+        increaseAgeButton.setOnClickListener((view -> increaseAge()));
+
+    }
+
+    private void increaseAge(){
+        user.setAge(user.getAge()+1);
+        ageTextView.setText(String.valueOf(user.getAge()));
+    }
+
 }
---
+++ app/src/main/java/com/stanzione/jetpackfeatures/User.java
@@ -0,0 +1,39 @@
+package com.stanzione.jetpackfeatures;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = 1;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
